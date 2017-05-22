316eed86ef3ace6b2036f9af002d8bb2ec7cac0e
Jack Bonime <jbonime@umich.edu>
2017-05-22 15:01:02 -0400

    hello

--- session.py
+++ session.py
@@ -1,15 +1,6 @@
-from flask import Flask, session
-import os
-
+from flask import Flask
 app = Flask(__name__)
-app.secret_key = os.urandom(24)
-
 
 @app.route('/')
-def index():
-    return 'Index'
-
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+def hello_world():
+    return 'Hello, World!'
